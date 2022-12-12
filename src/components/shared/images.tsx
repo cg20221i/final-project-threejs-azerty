6796704663c5a44e690c3a83bb0a15c2ee626c69
satria-ui <dimbum14@gmail.com>
2022-12-12 22:39:41 +0700

    feat: add image to scene, titles

--- src/components/shared/images.tsx
+++ src/components/shared/images.tsx
@@ -2,18 +2,18 @@ const pexel = (id) =>
   `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
 export const images = [
   // Front
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970), pages: '/theseus' },
+  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: 'img/kapal.webp', pages: '/Theseus' },
   // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: '/img/header.jpg', pages: '/about-us' },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452), pages: '/blob2' },
+  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: 'img/Frame2.png', pages: '/About-us' },
+  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: 'img/orang.webp', pages: '/Home' },
   // Left
-  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482), pages: '/blob2' },
-  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185), pages: '/blob2' },
-  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574), pages: '/blob2' },
+  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: 'img/kapalTurun.webp', pages: '/Gallery' },
+  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: 'img/tim.webp', pages: '/Team' },
+  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: 'img/IRC2018.webp', pages: '/Competition' },
   // Right
-  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675), pages: '/blob2' },
-  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738), pages: '/blob2' },
-  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986), pages: '/blob2' },
+  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: 'img/irc2017.webp', pages: '/Inamarine 2022' },
+  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: 'img/sponsor.webp', pages: '/Sponsors' },
+  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: 'img/cover.webp', pages: '/Contact' },
 ]
 
 // createRoot(document.getElementById('root')).render(<App images={images} />)
--- src/helper/frame.tsx
+++ src/helper/frame.tsx
@@ -27,8 +27,8 @@ export function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vecto
     }
   })
   useFrame((state, dt) => {
-    easing.damp3(state.camera.position, p, 0.4, dt, 5)
-    easing.dampQ(state.camera.quaternion, q, 0.4, dt, 5)
+    easing.damp3(state.camera.position, p, 0.3, dt, 5)
+    easing.dampQ(state.camera.quaternion, q, 0.3, dt, 5)
   })
   return (
     <group
@@ -79,7 +79,7 @@ export function Frame({ url, linkTo, c = new THREE.Color(), ...props }) {
         name={name}
         onPointerOver={(e) => (e.stopPropagation(), hover(true))}
         onPointerOut={() => hover(false)}
-        scale={[1, GOLDENRATIO, 0.05]}
+        scale={[1.2 , GOLDENRATIO, 0.05]}
         position={[0, GOLDENRATIO / 2, 0]}>
         <boxGeometry />
         <meshStandardMaterial color='#151515' metalness={0.5} roughness={0.5} envMapIntensity={2} />
@@ -87,7 +87,7 @@ export function Frame({ url, linkTo, c = new THREE.Color(), ...props }) {
           <boxGeometry />
           <meshBasicMaterial toneMapped={false} fog={false} />
         </mesh>
-        <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url} scale={2} />
+        <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url} scale={1} />
       </mesh>
       <mesh
         onClick={() => {
@@ -100,8 +100,8 @@ export function Frame({ url, linkTo, c = new THREE.Color(), ...props }) {
         <boxGeometry />
         <meshStandardMaterial color='#c28802' metalness={1} roughness={0.07} envMapIntensity={6} />
       </mesh>
-      <Text maxWidth={0.4} anchorX='center' anchorY='top' position={[0, 1.75, 0]} fontSize={0.125} strokeWidth={20}>
-        {image.toString()}
+      <Text maxWidth={1} anchorX='center' anchorY='top' position={[0, 1.75, 0]} fontSize={0.125} strokeWidth={20}>
+        {linkTo.replace("/", "")}
       </Text>
     </group>
   )
